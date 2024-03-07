796b0741a94ecf812a6ade58051f7a8e8a38e8e5
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-03-07 20:46:15 +0200

    plotting ok

--- src/chart/chart.cpp
+++ src/chart/chart.cpp
@@ -5,116 +5,156 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C Chart::display(U8G2_R0, U8X8_PIN_NONE);
 
 int Chart::max_price;
 price_data_t** Chart::data;
-
+time_t Chart::time_now;
+size_t Chart::data_now;
+size_t Chart::data_from;
+size_t Chart::data_to;
 
 void Chart::init() {
   display.begin();  
 };
 
+void Chart::set_time_and_data_ptrs() {
+  time_now = Networking::get_time();
+  data = Networking::price_data;
+  for (size_t i = 0; i < PRICES_MAX_NUM; i++) {
+    bool now = true;
+    now &= data[i]->year  == year(time_now);
+    now &= data[i]->month == month(time_now);
+    now &= data[i]->day   == day(time_now);
+    now &= data[i]->hour  == hour(time_now);
+    if (now) {
+      data_now = i;
+      data_from = i - 1;
+      data_to = i - 1 + PILLARS_NUM;
+      break;
+    }
+  }
+};
+
 void Chart::set_max_price() {
   max_price = 0;
-  for (size_t i = 0; i < PRICE_MAX_LEN; i++) {
+  for (size_t i = data_from; i < data_to; i++) {
     if (max_price < data[i]->cents_x100) {
       max_price = data[i]->cents_x100;
     }
   }
-  Serial.print("Max price: ");
-  Serial.println(max_price);
+  Serial.print("Max price: "); Serial.println(max_price);
+  // Add +1 to distinguish between 'max prices'
+  // It will be rounded, so f.ex. x.x, x.y, x.z would all show up as x
+  max_price += 100;
   // Upper limit 99, notice x100
+  // Lower limit 05
   if (max_price > 9900) {
     max_price = 9900;
-    Serial.println("Exceeded limit, changed to 9900.");
+    Serial.println("Max price exceeded limit, changed to 9900.");
+  } else if (max_price < 500) {
+    max_price = 500;
+    Serial.println("Max price below limit, changed to 500.");
   }
 };
 
-void Chart::draw_y_axis() {
-  // Y axis top label
-  char x10 = ((max_price / 100) / 10) + '0';
-  char x1  = ((max_price / 100) % 10) + '0';
-  char top[] = {x10, x1, '\0'};
-  display.drawStr(0, 6, top);
-  // Y axis bot label
-  display.drawStr(0, Y_PLOT_MAX, " 0");
-  // Y axis
-  display.drawLine(10, 0, 10, Y_PLOT_MAX);
+char* Chart::get_int_as_label(int val) {
+  // ASCII conversion
+  char x10 = (val / 10) + '0';
+  char x1  = (val % 10) + '0';
+  return new char[3]{x10, x1, '\0'};
 };
 
-void Chart::draw_x_axis() {
-  int x = 18;
-  for (size_t i = 0; i < 5; i++) {
-    int hour = data[i * 6 + 2]->hour;
-    // Hour to string
-    char x10 = (hour / 10) + '0';
-    char x1  = (hour % 10) + '0';
-    char h_str[] = {x10, x1, '\0'};
-    // Draw
-    display.drawStr(x, Y_MAX, h_str);
-    // Move X cursor
-    x += 24;
-  }
-  // X axis
-  display.drawLine(10, Y_PLOT_MAX, X_MAX, Y_PLOT_MAX);
+int Chart::get_pillar_height(int price) {
+  // Avoid losing information with int rounded, int < 1 is 0
+  return (((price * 100) / max_price) * Y_PLOT_MAX) / 100;
 };
 
-void Chart::draw_xy_mid() {
-  // Y axis mid label
-  char x10 = (((max_price / 2) / 100) / 10) + '0';
-  char x1  = (((max_price / 2) / 100) % 10) + '0';
-  char mid[] = {x10, x1, '\0'};
-  // Y axis mid position
-  // Adjust in case of rounded
-  int y_height = ((((max_price / 2) * 100) / max_price) * Y_PLOT_MAX) / 100;
-  int y = Y_PLOT_MAX - y_height;
-  display.drawStr(0, y + 2, mid);
-  // Draw along X axis
+void Chart::draw_y_axis_mid() {
+  // Y cursor
+  int height = get_pillar_height(max_price / 2);
+  int y = Y_PLOT_MAX - height;
+  // Draw axis horizontal
   display.drawLine(10, y, X_MAX, y);
+  // Draw mid label
+  char* mid_label = get_int_as_label((max_price / 2) / 100);
+  y += 2;
+  display.drawStr(0, y, mid_label);
+  delete[] mid_label;
 };
 
-void Chart::draw_pillar(int val, int x) {
-  int y_height = (((val * 100) / max_price) * Y_PLOT_MAX) / 100;
-  int y = Y_PLOT_MAX - y_height;
-  display.drawBox(x, y, 2, y_height);
+void Chart::draw_axis() {
+  // Draw top label
+  char* top_label = get_int_as_label(max_price / 100);
+  display.drawStr(0, 6, top_label);
+  delete[] top_label;
+  // Draw bottom label
+  display.drawStr(0, Y_PLOT_MAX, " 0");
+  // Draw Y axis
+  display.drawLine(10, 0, 10, Y_PLOT_MAX);
+  // Draw X axis
+  display.drawLine(10, Y_PLOT_MAX, X_MAX, Y_PLOT_MAX);
+  // Mid
+  draw_y_axis_mid();
+};
+
+void Chart::draw_x_label(int hour, int x) {
+  char* label = get_int_as_label(hour);
+  display.drawStr(x, Y_MAX, label);
+  delete[] label;
+};
+
+void Chart::draw_empty_pillar(int price, int x) {
+  int height = get_pillar_height(price);
+  int y = Y_PLOT_MAX - height;
+  // Draw box
+  display.drawLine(x, y, x + 3, y);
+  display.drawLine(x, y, x, Y_PLOT_MAX);
+  display.drawLine(x + 3, y, x + 3, Y_PLOT_MAX);
+};
+
+void Chart::draw_solid_pillar(int price, int x) {
+  int height = get_pillar_height(price);
+  int y = Y_PLOT_MAX - height;
+  // Draw box
+  display.drawBox(x, y, 4, height);
+};
+
+void Chart::draw_pillar(size_t index, int price, int x) {
+  if (index == data_now) {
+    draw_solid_pillar(price, x);
+  } else {
+    draw_empty_pillar(price, x);
+  }
 };
 
 void Chart::draw_pillars() {
   int x = 14;
-  for (size_t i = 0; i < 28; i++) {
-    int val = data[i]->cents_x100;
-    if (val > max_price) {
-      val = max_price;
+  for (size_t i = data_from; i < data_to; i++) {
+    int price = data[i]->cents_x100;
+    if (price > max_price) {
+      // Notice the upper limit of 99 c/kWh
+      price = max_price;
     }
-    draw_pillar(val, x);
-    x += 4;
+    draw_pillar(i, price, x);
+    // Draw label
+    if (i % 3 == 0) {
+      draw_x_label(data[i]->hour, x - 3);
+    }
+    // Move X cursor
+    x += 6;
   }
 };
 
+void Chart::draw_init() {
+  set_time_and_data_ptrs();
+  set_max_price();
+};
+
 void Chart::draw() {
-  data = Networking::price_data;
+  Serial.println("Initializing chart.");
+  draw_init();
+  Serial.print("Drawing...");
   display.clearBuffer();
   display.setFont(u8g2_font_5x7_tf);
-  set_max_price();
-  draw_y_axis();
-  draw_x_axis();
-  draw_xy_mid();
+  draw_axis();
   draw_pillars();
   display.sendBuffer();
-
-
-
- 
-  time_t time = Networking::get_time();
-  Serial.print("Time now: ");
-
-  int hours = hour(time);
-  int minutes = minute(time);
-  int seconds = second(time);
-  if (hours < 10) Serial.print("0");
-  Serial.print(hours);
-  Serial.print(":");
-  if (minutes < 10) Serial.print("0");
-  Serial.print(minutes);
-  Serial.print(":");
-  if (seconds < 10) Serial.print("0");
-  Serial.println(seconds);
-
+  Serial.println("Done.");
 };
--- src/chart/chart.h
+++ src/chart/chart.h
@@ -7,8 +7,15 @@
 
 #define X_MAX   127
 #define Y_MAX   63
-
 #define Y_PLOT_MAX  (Y_MAX - 8)
+
+#define PILLARS_NUM 19
+
+  /* 
+  * IDEAS
+  * - Show price Now and Day Average?
+  * - Follow actual price with mid line?
+  */
 
 class Chart {
 public:
@@ -22,12 +29,22 @@ private:
   static U8G2_SH1106_128X64_NONAME_F_HW_I2C display;
   static int max_price;
   static price_data_t** data;
+  static time_t time_now;
+  static size_t data_now;
+  static size_t data_from;
+  static size_t data_to;
 
   // Functions
+  static void set_time_and_data_ptrs();
   static void set_max_price();
-  static void draw_y_axis();
-  static void draw_x_axis();
-  static void draw_xy_mid();
-  static void draw_pillar(int val, int x);
-  static void draw_pillars();  
+  static char* get_int_as_label(int val);
+  static int get_pillar_height(int price);
+  static void draw_y_axis_mid();
+  static void draw_axis();
+  static void draw_x_label(int hour, int x);
+  static void draw_empty_pillar(int price, int x);
+  static void draw_solid_pillar(int price, int x);
+  static void draw_pillar(size_t index, int price, int x);
+  static void draw_pillars();
+  static void draw_init();
 };
