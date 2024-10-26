a4ac16cb02262843df52a7fc27157fb395f802f6
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-10-26 05:15:42 +0300

    tests

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,18 @@ void start_serial() {
     ; // Wait until serial port connected
   }
   delay(100);
-  Serial.println("Started Serial.");
+  Serial.println("Main: Serial started.");
 };
 
 void setup() {
   start_serial();
-  Chart::init();
+  //Chart::init();
   Networking::enable();
 };
 
 void loop() {  
-  Networking::update_data();
-  Chart::draw();
+  //Networking::update_data();
+  //Chart::draw();
 
   delay(2000);
 
@@ -39,4 +39,28 @@ void loop() {
   // Show error on screen?
 
   delay(2000);
+
+  time_t time_now;
+  Networking::get_time(&time_now);
+  Serial.print("Main: Time now: ");
+  Serial.println(time_now);
+
+  delay(2000);
+
+  price_data_t* data;
+  Networking::get_data(&data);
+
+  Serial.print("\n\n");
+  price_data_t* temp;
+  while (data != nullptr) {
+    // Print
+    Serial.print("Time: "); Serial.print(data->time);
+    Serial.print(" Price: "); Serial.println(data->cents_x100);
+    // Delete
+    temp = data->next;
+    delete data;
+    data = temp;
+  }
+  Serial.print("\n\n");
+
 };
