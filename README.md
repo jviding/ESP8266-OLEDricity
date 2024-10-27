151188828167503c489a225d2a1ed3354921ae66
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-10-27 21:13:42 +0200

    ok

--- README.md
+++ README.md
@@ -2,12 +2,27 @@
 Show electricity prices with 1.3" OLED display.
 
 
-# Find RPi
-sudo nmap --open -sS -p 22 10.55.134.0/24
+## Flashing
 
-# Flash from RPi
-esptool.py --chip esp8266 --port /dev/ttyS0 write_flash -z 0x00000 firmware.bin
+Find RPi:
+\$ sudo nmap --open -sS -p 22 10.55.134.0/24
 
-# Listen from RPi
-minicom -D /dev/ttyACM0 -b 115200
+USB:
+\$ esptool.py --chip esp8266 --port /dev/ttyUSB0 write_flash -z 0x00000 firmware.bin
 
+Serial:
+\$ esptool.py --chip esp8266 --port /dev/ttyS0 write_flash -z 0x00000 firmware.bin
+
+Serial pins:
+
+RPi   -   ESP8266
+
+3.3V  -   Vcc
+GND   -   GND
+RX    -   U0TXD
+TX    -   U0RXD
+      -   EN      (pull-up 10k)
+      -   GPIO 15 (pull-down 10k)
+      -   GPIO 0  (pull-down 10k)
+      
+For normal boot operation, switch GPIO 0 to pull-up.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "Arduino.h"
 #include "networking/networking.h"
-#include "chart/chart.h"
+#include "displays/displays.h"
+//#include "led/led.h"
+//#include "time.h"
 
-#include "time.h"
+ /* 
+  * IDEAS
+  * - Show price Now and Day Average?
+  * - Follow actual price with mid line?
+  * - Pull-down to display sclk?
+  */
+
+  // Use set address?
+  // Fixes problems with Mosfet?
 
 
 // CHECK HOW TO DELETE OBJECT, INSTEAD OF POINTER
@@ -28,21 +38,58 @@ void start_serial() {
   Serial.println("Main: Serial started.");
 };
 
+
 void setup() {
   start_serial();
   //Chart::init();
+  //Led::init();
+  //Displays::init();
   Networking::enable();
+};
+
+void debug_print_heap() {
+  uint32_t free = ESP.getFreeHeap();
+  Serial.print("Free heap: ");
+  Serial.println(free);
 };
 
 void loop() {  
   //Networking::update_data();
   //Chart::draw();
+  // Update prices and time
+  //Networking::update_data();
+  //price_data_t** price_data = Networking::price_data;
+  //uint32_t time_now = Networking::get_time();
+  // Draw to displays
+  //Displays::draw(price_data, time_now);
 
-  delay(2000);
+  delay(5000);
 
-  uint32_t free = ESP.getFreeHeap();
-  Serial.print("Free heap: ");
-  Serial.println(free);
+  // Leds
+
+  /*delay(500);
+  Led::blue();
+  delay(500);
+  Led::green();
+  delay(500);
+  Led::yellow();
+  delay(500);
+  Led::red();*/
+
+  // Wait 1h -> Draw next hour
+  // Wait 12h -> new data
+ 
+  /*Serial.print("Time: "); 
+  Serial.print(year(time_now));
+  Serial.print("-");
+  Serial.print(month(time_now));
+  Serial.print("-");
+  Serial.print(day(time_now));
+  Serial.print("-T");
+  Serial.print(hour(time_now));
+  Serial.println("");*/
+
+  Serial.println("Hello");
 
   // Then disconnect WiFI for 12h or so
   // If this fails, retry and x2 the wait if fails
