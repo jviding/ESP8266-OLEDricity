3952434db357db19a2d5252d87881b9aa5726914
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-10-30 01:45:37 +0200

    code improvement

--- src/networking/networking.cpp
+++ src/networking/networking.cpp
@@ -8,30 +8,30 @@ bool Networking::enable() {
     return false;
   } else {
     HTTPS_req::enable();
-    Serial.println("Network: Enabled.");
+    Serial.println("Network: Started.");
     delay(500);
     return true;
   }  
 };
 
 void Networking::disable() {
-  Serial.println("Network: Disconnecting...");
+  Serial.println("Network: Disconnect.");
   wifi_disconnect();
 }
 
 bool Networking::get_time(time_t* time) {
-  Serial.println("Network: Getting time...");
+  Serial.println("Network: Get time.");
   wifi_ensure_is_connected();
   return NTP_time::get_finnish_time(time);
 };
 
 bool Networking::get_data(price_data_t** data) {
-  Serial.println("Network: Getting price data...");
+  Serial.println("Network: Get price data.");
   wifi_ensure_is_connected();
   https_t* raw_data;
   bool res_ok = true;
-  res_ok &= HTTPS_req::get_data(&raw_data);
-  res_ok &= Parser::get_price_data(&raw_data, data);
+  res_ok &= HTTPS_req::get_data(&raw_data) != -1;
+  res_ok &= Parser::get_price_data(raw_data, data);
   if (!res_ok) {
     Serial.println("Network: Failed.");
   }
