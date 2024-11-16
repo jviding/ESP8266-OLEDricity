1d966dd1796dbb2a33d6d46a7f2d2b31340791a0
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-11-16 02:05:22 +0200

    wifi tests

--- src/tests/networking/wifi_ctrl/wifi_test.cpp
+++ src/tests/networking/wifi_ctrl/wifi_test.cpp
@@ -1,5 +1,13 @@
 #include "wifi_test.h"
 
-void WiFi_test::test_eeprom() {
-  Eeprom_test::test();
+bool WiFi_test::test_eeprom() {
+  return Eeprom_test::test_read_write();
+};
+
+bool WiFi_test::test_server_over_WiFi() {
+  Serial.println("Test server over WiFi: Connect");
+  WiFi_ctrl::connect();
+  Serial.println("Test server over WiFi: Connected"); 
+  delay(500);
+  return Server_test::test_web_service();
 };
--- src/tests/networking/wifi_ctrl/wifi_test.h
+++ src/tests/networking/wifi_ctrl/wifi_test.h
@@ -1,15 +1,14 @@
 #pragma once
 
 #include <Arduino.h>
-#include "eeprom_ctrl/eeprom_test.h"
+#include "../../../networking/wifi_ctrl/wifi_ctrl.h"
 
-//#include "../../../../networking/wifi_ctrl/eeprom_ctrl/eeprom_ctrl.h"
+#include "eeprom_ctrl/eeprom_test.h"
+#include "server_ctrl/server_test.h"
 
 
 class WiFi_test {
 public:
-  static void test_eeprom();
-
-private:
-  static bool some();
+  static bool test_eeprom();
+  static bool test_server_over_WiFi();
 };
