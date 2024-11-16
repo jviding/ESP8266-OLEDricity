ef8b177786b8894d3bc763ffe12520eab4ea6834
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-11-16 22:21:16 +0200

    displays tests

--- src/tests/displays/displays_test.cpp
+++ src/tests/displays/displays_test.cpp
@@ -2,19 +2,17 @@
 
 
 void Displays_test::test_displays_with_data_fast_loop() {
-
-
-  /*bool res_ok = true;
-  // Time
+  bool res_ok;
+  // Get time
   int time_now = 0;
-  res_ok &= Networking::get_time(&time_now);
-  // Data
+  res_ok = Networking::get_time(&time_now);
+  // Get data
   price_data_t* data = nullptr;
   res_ok &= Networking::get_data(&data);
-  // Loop until no data
+  // Loop until no data to display
   while (res_ok) {
     // Draw
-    res_ok = Displays::draw(data, time_now) != 0;
+    res_ok &= Displays::draw(data, time_now) != 0;
     // Next hour
     if (time_now % 100 == 23) {
       time_now += 100 - 23;
@@ -29,5 +27,24 @@ void Displays_test::test_displays_with_data_fast_loop() {
     price_data_t* temp = data->next;
     delete data;
     data = temp;
-  }*/
+  }
+};
+
+bool Displays_test::enable_networking_for_real_data() {
+  char* ip_address = nullptr;
+  char* password = nullptr;
+  bool res_ok = true;
+  if (!Networking::enable()) {
+    Serial.println("## Test Displays: Set WiFi credentials.");
+    res_ok = Networking::hotspot_enable(&ip_address, &password);
+    res_ok = res_ok && Networking::set_WiFi_SSID_and_password();
+    res_ok = res_ok && Networking::hotspot_disable();
+  }
+  delete[] ip_address;
+  delete[] password;
+  if (!res_ok) {
+    Serial.println("## Test Displays: Failed enabling real data use");
+    delay(500);
+  }
+  return res_ok;
 };
--- src/tests/displays/displays_test.h
+++ src/tests/displays/displays_test.h
@@ -2,9 +2,10 @@
 
 #include <Arduino.h>
 #include "../../displays/displays.h"
-
+#include "../../networking/networking.h"
 
 class Displays_test {
 public:
   static void test_displays_with_data_fast_loop();
+  static bool enable_networking_for_real_data();
 };
