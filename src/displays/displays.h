07f9487f4d5125d3a18641a4554f357ec122a0ef
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-11-20 03:45:20 +0200

    remove dataset funcs

--- src/displays/displays.h
+++ src/displays/displays.h
@@ -30,11 +30,4 @@ private:
   static void init_chart();
   static void draw_banner(int price_now);
   static void draw_chart(dataset_t* dataset);
-
-  // DATASET
-  // Functions
-  static bool set_ptr_to_data_now(dataset_t* dataset, price_data_t* data, int time_now);
-  static void try_shift_data_ptr_one_left(dataset_t* dataset);
-  static bool set_price_max(dataset_t* dataset, int dataset_size);
-  static int create_dataset(dataset_t** dataset, price_data_t* data, int time_now);
 };
