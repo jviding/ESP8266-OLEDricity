8c50ce91bdaba4dbc4c323e20ee06a0cf9e85487
jasu <jasu@jasubox.myguest.virtualbox.org>
2025-01-11 00:27:58 +0200

    fix func name in call

--- src/tests/displays/chart/chart_test.cpp
+++ src/tests/displays/chart/chart_test.cpp
@@ -5,7 +5,7 @@ void Chart_test::test_write_hotspot_message() {
   char* name = new char[12]{'E','l','e','c','t','r','i','c','u','b','e','\0'};
   char* pwd = new char[9]{'8','8','8','8','8','8','8','8','\0'};
   char* ip = new char[16]{'2','5','5','.','2','5','5','.','2','5','5','.','2','5','5','\0'};
-  Displays::chart_write_hotspot_messages(name, pwd, ip);
+  Displays::write_msg_hotspot(name, pwd, ip);
   delete[] name;
   delete[] pwd;
   delete[] ip;
